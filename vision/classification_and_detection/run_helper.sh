2b6a8e60f517c0afda72f8b9a7b31a13eb15992e
manyi <manyi@localhost>
2023-06-27 13:46:57 -0600

    fix: entry to main

--- vision/classification_and_detection/run_helper.sh
+++ vision/classification_and_detection/run_helper.sh
@@ -12,7 +12,7 @@ start_fmt=$(date +%Y-%m-%d\ %r)
 echo "STARTING RUN AT $start_fmt"
 
 # cd /mlperf
-python ./python/main.py $opts --output /output
+python /tmp/mlperf_inference/vision/classification_and_detection/python/main.py $opts --output /output
 
 end_fmt=$(date +%Y-%m-%d\ %r)
 echo "ENDING RUN AT $end_fmt"
